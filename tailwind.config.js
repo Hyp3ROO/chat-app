f4a609772e4b3c1866b02b281598b0ff29dc602f
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-05-25 16:04:33 +0200

    initialized vite app

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'primary-bg': '#051923',
+        'secondary-bg': '#00a6fb',
+        primary: '#003554',
+        secondary: '#006494',
+        hover: '#0582ca',
+      },
+    },
+  },
+  plugins: [],
+}
