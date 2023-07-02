11cb60a24632b05b66015278728cdc1e2659c8f8
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-07-02 23:35:30 +0200

    created Profile page

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar/Navbar'
+import ProfileInfo from '../components/Profile/ProfileInfo'
+
+const Profile = () => {
+  return (
+    <>
+      <Navbar />
+      <ProfileInfo />
+    </>
+  )
+}
+export default Profile
