7cabf3c0d3e200154ebc4a2799281f341988b08d
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-07-02 23:34:34 +0200

    updated main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import AuthContextProvider from './context/AuthContext.tsx'
+import ChatContextProvider from './context/ChatContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </ChatContextProvider>
+  </AuthContextProvider>
 )
