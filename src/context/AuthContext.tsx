c18a03fb121cc0cb9caba10fd1ee5770dae9a69b
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-07-02 23:34:17 +0200

    created AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+import { User, onAuthStateChanged } from 'firebase/auth'
+import { createContext, useEffect, useState } from 'react'
+import { auth } from '../auth/firebase'
+
+type AuthContextType = {
+  currentUser: User | null
+  authLoaded: boolean
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  currentUser: null,
+} as AuthContextType)
+
+const AuthContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+  const [authLoaded, setAuthLoaded] = useState(false)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user)
+      setAuthLoaded(true)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ currentUser, authLoaded }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+export default AuthContextProvider
