8e33a30af4430ddf3a5a2d61332cea278c06f47f
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-08-06 21:42:36 +0200

    changed z-index

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar'
 
 const Sidebar = () => {
   return (
-    <div className='fixed z-20 flex w-full flex-col md:static md:max-h-screen md:w-1/3'>
+    <div className='fixed flex w-full flex-col md:static md:max-h-screen md:w-1/3 z-50'>
       <Navbar />
       <Chats />
     </div>
