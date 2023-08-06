d0a5bf54f1102caedbb06f98e49d94c731609325
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-08-06 21:44:51 +0200

    created MessageImage

---
+++ src/components/Chatbox/MessageImage.tsx
@@ -0,0 +1,22 @@
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
+type MessageImageProps = {
+  image: string
+  [key: string]: unknown
+}
+
+const MessageImage = ({ image, ...rest }: MessageImageProps) => {
+  return (
+    <Zoom zoomMargin={4}>
+      <img
+        src={image}
+        alt=''
+        className='rounded-lg object-cover'
+        loading='lazy'
+        {...rest}
+      />
+    </Zoom>
+  )
+}
+export default MessageImage
