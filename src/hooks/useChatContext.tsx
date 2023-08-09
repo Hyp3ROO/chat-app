76609a12e9bc0668e347c8d7cb967d0229b5811c
Hyp3ROO <mateuszjurkiewicz1010@gmail.com>
2023-08-09 12:56:19 +0200

    created useChatContext

---
+++ src/hooks/useChatContext.tsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import { ChatContext } from '../context/ChatContext'
+
+const useChatContext = () => {
+  const chatContext = useContext(ChatContext)
+  if (!chatContext)
+    throw new Error(
+      'No ChatContext.Provider found when calling useChatContext.'
+    )
+  return chatContext
+}
+export default useChatContext
